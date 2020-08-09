8c10ee659819e6000ddfb3268bba47bddd5e9737
Mondrovic <mondrovic1@gmail.com>
2020-08-09 09:29:54 -0700

    setup basic JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
+// prompts user for input 
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
+
+// window references the browser and alert is a built in function to create a dialogue box
 window.alert("This is an alert! JavaScript is running!");
+
+// create custom function
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
