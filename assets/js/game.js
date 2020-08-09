dccec64d17b338eaa59c885104b0095188ad0e26
Mondrovic <mondrovic1@gmail.com>
2020-08-09 09:53:22 -0700

    added game core, still need to add loop and conditionals

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,32 @@
-// prompts user for input 
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10+10);
-console.log("Our robot's name is " + playerName);
+console.log(playerName, playerAttack, playerHealth);
 
-// window references the browser and alert is a built in function to create a dialogue box
-window.alert("This is an alert! JavaScript is running!");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-// create custom function
-function fight() {
-    window.alert("The fight has begun!");
-}
+// main code block
+var fight = function(){
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    if (enemyHealth <= 0){
+        window.alert(enemyName + " has died!");
+    }
+    else{
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    playerHealth = playerHealth - enemyAttack;
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    if (playerHealth <= 0){
+        windows.alert(playerName + " has died!");
+    }
+    else{
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+};
 
-// fight();
+fight();
